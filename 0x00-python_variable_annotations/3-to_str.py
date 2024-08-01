6b542cbbef81e12c55773096879984a16b612d22
Iyanu Dayisi <tobiy23@gmail.com>
2024-08-02 00:46:10 +0100

    to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Typed Functions"""
+
+
+def to_str(n: float) -> str:
+    """a typed floor function that takes in a float
+    and returns the string representation."""
+    return str(n)
