beb6e48cde9b876fd0f49276165f878c47018367
Iyanu Dayisi <tobiy23@gmail.com>
2024-08-02 00:39:35 +0100

    create floor function

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Typed Functions"""
+import math
+
+def floor(n: float) -> int:
+    """a typed floor function that takes in a float
+    and returns the floor value"""
+    return math.floor(n)
