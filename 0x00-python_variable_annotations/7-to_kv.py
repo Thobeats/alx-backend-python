ab3ffd9892bf80800cf261bb67c330f824a45de4
Iyanu Dayisi <tobiy23@gmail.com>
2024-08-02 01:26:11 +0100

    tuple typing

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -6,4 +6,4 @@ from typing import Union, Tuple
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """A function that sums a list of floats"""
-    return [k, v*v]
+    return (k, v*v)
