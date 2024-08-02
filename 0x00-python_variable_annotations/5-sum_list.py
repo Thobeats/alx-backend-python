9e7ba8261d9a944d8b2c925117bbec33fc1a44a0
Iyanu Dayisi <tobiy23@gmail.com>
2024-08-02 01:09:23 +0100

    sum list

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -3,6 +3,6 @@
 from functools import reduce
 
 
-def sum_list(input_list: list) -> float:
+def sum_list(input_list: list[float]) -> float:
     """A function that sums a list of floats"""
     return reduce(lambda agg, num: agg + num, input_list)
