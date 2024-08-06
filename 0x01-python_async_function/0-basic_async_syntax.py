2c9c8a577b18b1e6df2b4041142e85af72cf0d53
Iyanu Dayisi <tobiy23@gmail.com>
2024-08-06 07:20:17 +0100

    async and await

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,7 @@ from asyncio import run, sleep
 from random import uniform
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int=10) -> float:
     """A function to generate a random number"""
     delay = uniform(0, max_delay)
     await sleep(delay)
