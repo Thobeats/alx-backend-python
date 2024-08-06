33d2b8a64cc50bf3b87e4fd4d72d2ea2e9badace
Iyanu Dayisi <tobiy23@gmail.com>
2024-08-06 07:30:40 +0100

    basic async module

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -2,11 +2,11 @@
 """A basic aync module"""
 
 from asyncio import run, sleep
-from random import random
+from random import uniform
 
 
-async def wait_random(max_delay: int=10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """A function to generate a random number"""
-    delay = random() * max_delay
+    delay = uniform(0, max_delay)
     await sleep(delay)
     return delay
